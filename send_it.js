73851f9a4f6c3bdb7124d302ce9ecf820def6f8d
Robiq <robin@wub.no>
2017-07-24 13:44:41 +0900

    Added assert-functionality!

--- send_it.js
+++ send_it.js
@@ -236,6 +236,26 @@ pc2.onicegatheringstatechange = onicegatheringstatechange
 
 pc2.onconnection = handleOnconnection
 
+//Source: https://gist.github.com/jeromeetienne/2651899
+/**
+ * A console.assert which actually stop the exectution.
+ * default console.assert() is a plain display, such as console.log() or console.error();
+ * It doesnt stop the execution like assert() is meant to do. This is a little code to 
+ * "workaround this limitation" :) thanks @jensarp
+ *
+ * Usage:
+ * console.assert(foo === bar); // Will throw if not equal
+ * console.assert(foo === bar, 'Dude, foo does not equal bar'); // Will throw with custom error message
+ *
+ * To trigger js debugger on failed assert, do 
+ * console.assert.useDebugger = true;
+*/
+console.assert  = function(cond, text){
+  if( cond )  return;
+  if( console.assert.useDebugger )  debugger;
+  throw new Error(text || "Assertion failed!");
+};
+
 //My own!------------------------------------
 function isReady(){
   if (fileReady && iceReady && mailReady) {
