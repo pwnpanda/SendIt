b23c2b9b7746bd019bb83fb8c81c6032a7b1b942
Robiq <robin@wub.no>
2017-07-24 13:47:50 +0900

    Added functionality to handle authentication setup and authentication setup replies

--- myCrypto.js
+++ myCrypto.js
@@ -40,36 +40,64 @@ function readCrypto(){
 }
 //Try to authenticate user
 function beginAuth (){
+  console.log("Starting authentication process!")
   var key = KeyManager.findKey(KeyManager.otherEnd);
   //if we have the receivers key in the list:
   if(key != null){
+  	console.log("Creating challenge!")
     //create challenge and tempHash
     KeyManager.generateRandom();
     //Tell Protocol to send an authentication challenge!
     createAuthMsg(protocol.AUTH_CHALLENGE);
   } else {
   //If setup:
+  	console.log("starting setup!");
     //Tell Protocol to setup authentication for next time
     createAuthMsg(protocol.AUTH_SETUP);
   }
 }
 //Process the reply from receiver and authenticate/deny connection
 function processAuth(reply){
-  //If challenge:
-  	//Decrypt challenge with private key
-  	//Calculate hash
-  	//Encrypt with sender's key
-  //If challenge-reply:
-    //Decrypt reply with private key
-    //Compare decrypted reply and temp hash
-    //Call stageFiles(); or throw error!
-  //If setup
-    //Store e-mail and public key
-    //Reply with own e-mail and public key
-  //If setup-reply
-    //Call stageFiles();
-    //Assert e-mail
-    return;
+  switch(reply.action){
+  	    
+    case protocol.AUTH_CHALLENGE:
+      //Received an authentication challenge
+      	//Decrypt challenge with private key
+	  	//Calculate hash
+	  	//Encrypt with sender's public key
+      	//Send AUTH_RESPONSE
+    	break;
+
+    //Received an authentication response
+    case protocol.AUTH_RESPONSE:
+	    //Decrypt reply with private key
+		//Compare decrypted reply and temp hash
+		//Call stageFiles(); or throw error!
+    	break;
+    
+    //Received authentication setup information  
+    case protocol.AUTH_SETUP:
+    	KeyManger.otherEnd = reply.sender;
+    	//Store e-mail and public key
+    	KeyManager.storeKey(reply.sender, reply.key);
+    	//Reply with own e-mail and public key in AUTH_S_REPLY
+    	createAuthMsg(protocol.AUTH_S_REPLY);
+    	break;
+    //Received authentication setup reply
+    case protocol.AUTH_S_REPLY:
+    	//Assert e-mail
+    	console.assert((reply.sender === KeyManager.otherEnd), "Receivers e-mail is not correct! Security breach found! Terminating!");
+    	//Store e-mail and public key
+    	KeyManager.storeKey(reply.sender, reply.key);
+    	//Authentication setup complete - start transfer!
+    	console.log("Authentication setup complete! Starting transfer!");
+    	stageFiles();
+    	break;
+    //Error!
+    default: 
+    	console.error("Malformed message type: ", reply.action);
+    	break;
+  }
 }
 //Behaviour for writing keymanager to cryptoFile
 function writeCrypto(){
