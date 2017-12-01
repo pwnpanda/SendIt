c12c1a5d31216151d14255babdc00aebbd79bed9
Robiq <robin@wub.no>
2017-12-02 01:15:53 +0900

    bugfix mac

--- Electron/init.js
+++ Electron/init.js
@@ -8,7 +8,7 @@
 var electron = require('electron'); // http://electron.atom.io/docs/api
 var path = require('path');         // https://nodejs.org/api/path.html
 var url = require('url');        // https://nodejs.org/api/url.html
-var {app, BrowserWindow} = electron;
+var {app, BrowserWindow, Menu} = electron;
 
 var window = null
 
@@ -23,23 +23,41 @@ app.once('ready', function () {
     // Set the default background color of the window to match the CSS
     // background color of the page, this prevents any white flickering
     backgroundColor: "#75B19F",
+    frame: "false",
     // Don't show the window until it ready, this prevents any white flickering
     show: false,
     title: "Send It",
-    icon: path.join(__dirname, 'icon.png')
-  })
+    icon: path.join(__dirname, 'icon.png'),
+  });
 
   // Load a URL in the window to the local index.html path
   window.loadURL(url.format({
     pathname: path.join(__dirname + '/resources/', 'send_it.html'),
     protocol: 'file:',
     slashes: true
-  }))
+  }));
+
+var template = [{
+    submenu: [
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+    ]}
+];
+
 
   // Show window when page is ready
   window.once('ready-to-show', function () {
     window.maximize();
-    window.setMenu(null);
+    if (process.platform === 'darwin') { 
+      Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+    }else{
+      window.setMenu(null);
+    }
     window.show()
   })
 })
